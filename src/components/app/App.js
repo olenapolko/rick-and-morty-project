801fc435d3703f2584f5f5d32df6c9931a39da06
olenapolko <olenapolko@gmail.com>
2023-03-20 04:09:25 +0200

    added hashrouter to index.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,32 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import { lazy, Suspense } from 'react';
+// import Spinner from '../spinner/Spinner';
+
+// const Login = lazy(() => import('../pages/Login'));
+// const Page404 = lazy(() => import('../pages/404'));
+// const MainPage = lazy(() => import('../pages/MainPage'));
+// const CharPage = lazy(() => import('../pages/CharPage'));
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="app">
+//         <Suspense fallback={<Spinner/>}>
+//           <Routes>
+//             <Route path="/" element={<Login />} />
+//             <Route path="/characters" element={<MainPage/>}/>
+//             <Route path="/characters/:charId" element={<CharPage />} />
+//             <Route path="*" element={<Page404/>} />
+//           </Routes>
+//         </Suspense>
+//       </div>
+//     </Router>
+//   )
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from 'react';
 import Spinner from '../spinner/Spinner';
 
@@ -9,7 +37,6 @@ const CharPage = lazy(() => import('../pages/CharPage'));
 
 function App() {
   return (
-    <Router>
       <div className="app">
         <Suspense fallback={<Spinner/>}>
           <Routes>
@@ -20,8 +47,8 @@ function App() {
           </Routes>
         </Suspense>
       </div>
-    </Router>
   )
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './components/app/App';
+// import './style/style.scss';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createHashRouter, RouterProvider} from 'react-router-dom';
 import App from './components/app/App';
 import './style/style.scss';
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
